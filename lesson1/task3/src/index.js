4b8558a66f0e3b0893050b61537a78ad95ba668b
mariia <rmashka2001@gmail.com>
2021-10-03 19:33:56 +0300

    4

--- lesson1/task3/src/index.js
+++ lesson1/task3/src/index.js
@@ -1,31 +1,38 @@
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles.css';
 
 const rootElement = document.querySelector('#root');
-const element = ( <
-    div class = "greeting" >
-    <
-    div class = "greeting__title" > Hello, world! < /div> <
-    div class = "greeting__text" > I 'm learning React</div> <
-    /div>
-);
 
-
-ReactDom.render(element, rootElement);
 // const renderGreeting = elem => {
-//     const containerElem = document.createElement('div');
-//     containerElem.classList.add('greeting');
+//   const containerElem = document.createElement('div');
+//   containerElem.classList.add('greeting');
 
-//     const titleElem = document.createElement('h1');
-//     titleElem.classList.add('greeting__title');
-//     titleElem.textContent = 'Hello, world!';
+//   const titleElem = document.createElement('h1');
+//   titleElem.classList.add('greeting__title');
+//   titleElem.textContent = 'Hello, world!';
 
-//     const textElem = document.createElement('p');
-//     textElem.classList.add('greeting__text');
-//     textElem.textContent = "I'm learning React";
+//   const textElem = document.createElement('p');
+//   textElem.classList.add('greeting__text');
+//   textElem.textContent = "I'm learning React";
 
-//     containerElem.append(titleElem, textElem);
+//   containerElem.append(titleElem, textElem);
 
-//     elem.append(containerElem);
+//   elem.append(containerElem);
 // };
 
 // renderGreeting(rootElement);
+
+
+
+
+const element = ( <
+    div className = "greeting" >
+    <
+    h1 className = "greeting__title" > Hello, world! < /h1> <
+    p className = "greeting__text" > I 'm learning React</p> <
+    /div>
+);
+
+ReactDOM.render(element, rootElement);
