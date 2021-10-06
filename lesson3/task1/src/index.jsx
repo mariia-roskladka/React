90c355e735fd0fa8c1f50cbf8e92c0eb31c2c2e4
mariia <rmashka2001@gmail.com>
2021-10-06 14:24:59 +0300

    3,1

--- lesson3/task1/src/index.jsx
+++ lesson3/task1/src/index.jsx
@@ -4,19 +4,19 @@ import './index.scss';
 import './search.scss';
 
 const rootElement = document.querySelector('#root');
-
-const Greeting=()=>{
+const SearchField=(props)=>{
+    console.log(props);
    
-    const element=(
-        <div class="search">
-    <h1 class="search__title">Hello, Tom. What to search for you?</h1>
-    <div class="search__field">
-        <input type="text" class="search__input"/>
-        <button class="search__button">Search</button>
-    </div>
-</div>
-);
-return element;
+    return (
+        <div className="search">
+            <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+                <input type="text" className="search__input"/>
+                <button className="search__button">Search</button>
+            </div>
+        </div>
+    );
+   
 };
 
-ReactDOM.render(<Greeting/>, rootElement);
+ReactDOM.render(<SearchField name="Mariia" age={20}/>, rootElement);
