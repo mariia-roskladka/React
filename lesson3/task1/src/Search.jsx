7914a626b80d9d7839c55f2998b959f1adfa0fc3
mariia <rmashka2001@gmail.com>
2021-10-06 14:52:08 +0300

    3.1

--- lesson3/task1/src/Search.jsx
+++ lesson3/task1/src/Search.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 
 export default props => {
   console.log(props);
@@ -13,17 +12,3 @@ export default props => {
     </div>
   );
 };
-=======
-import ReactDOM from 'react-dom';
-import Search from './Search.jsx';
-import './index.scss';
-import './search.scss';
-
-const rootElement = document.querySelector('#root');
-
-<<<<<<<< HEAD:lesson3/task1/src/Search.jsx
-ReactDOM.render(<SearchField name="Mariia" age={20}/>, rootElement);
-========
-ReactDOM.render(<Search name="Bob" age={17} />, rootElement);
->>>>>>>> ac708ceae9f0ed77c550a372fecde80b5b199db3:lesson3/task1/src/index.jsx
->>>>>>> ac708ceae9f0ed77c550a372fecde80b5b199db3
