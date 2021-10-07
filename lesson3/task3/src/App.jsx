8cce4baee22f61806e5bbf04d7dbf07b81b338e6
mariia <rmashka2001@gmail.com>
2021-10-07 19:22:14 +0300

    3

---
+++ lesson3/task3/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Comment from './Comment.jsx';
+
+const userInfo = {
+  name: 'Tom',
+  avatarUrl: 'https://avatars1.githubusercontent.com/users',
+};
+
+const App = () => {
+  return <Comment author={userInfo} text="Good job" date={new Date()} />;
+};
+export default App;
---
+++ lesson3/task3/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+import './styles.scss';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<App />, rootElement);
