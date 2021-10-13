22ce132267c5e6f4de44a4d4033a008f434c293e
mariia <rmashka2001@gmail.com>
2021-10-13 11:56:59 +0300

    7.2

---
+++ lesson7/task2/src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import NumberList from './NumbersList.jsx';
+
+
+const rootElement=document.querySelector('#root');
+
+
+const numbers=[1, 2, 3, 4, 5];
+
+ReactDOM.render(<NumberList numbers={numbers}/>, rootElement);
