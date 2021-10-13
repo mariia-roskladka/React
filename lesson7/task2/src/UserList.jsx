3ab919a9225eb97cba83a0a1e7b7329df98e9ec2
mariia <rmashka2001@gmail.com>
2021-10-13 17:16:01 +0300

    7.2

--- lesson7/task2/src/UserList.jsx
+++ lesson7/task2/src/UserList.jsx
@@ -1,14 +1,43 @@
-import React from "react";
-import User from "./User.jsx";
+import React, { Component } from 'react';
+import User from './User.jsx';
 
-const UserList=({users})=>{
-    return(
+class UsersList extends Component {
+  state = {
+    sorting: null,
+  };
+
+  toggleSorting = () => {
+    const newSorting = this.state.sorting === 'asc' ? 'desc' : 'asc';
+    this.setState({
+      sorting: newSorting,
+    });
+  };
+
+  render() {
+    // console.log(this.props.users);
+    let usersList;
+    if (this.state.sorting) {
+      usersList = this.props.users
+        .slice()
+        .sort((a, b) => (this.state.sorting === 'asc' ? a.age - b.age : b.age - a.age));
+    } else {
+      usersList = this.props.users;
+    }
+
+    return (
+      <div>
+        <button className="btn" onClick={this.toggleSorting}>
+          {this.state.sorting || '-'}
+        </button>
+
         <ul className="users">
-            {users.map(user=>(
-                <User key={user.name} name={user.name} age={user.age} />
-            ))}
+          {usersList.map(user => (
+            <User key={user.id} /*name={user.name} age={user.age}*/ {...user} />
+          ))}
         </ul>
+      </div>
     );
+  }
 }
 
-export default UserList;
+export default UsersList;
