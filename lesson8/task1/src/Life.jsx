f1dd78c6436c7cc5358097dc792ab023ce25a47f
mariia <rmashka2001@gmail.com>
2021-10-15 11:29:59 +0300

    8.1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -3,24 +3,23 @@ import React, {Component} from "react";
 class Life extends Component{
     constructor(props){
         super(props);
-        console.log('=> constructor');
+        console.log('constructor: good place to create state');
     }
 
     componentDidMount(){
-        console.log('=> componentDidMount');
+        console.log('componentDidMount: API calls, subscriptions');
     }
     shouldComponentUpdate(nextProps, nextState){
-        console.log('=> shouldComponentUpdate');
-        return nextProps.number%2;
+        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
     }
     componentDidUpdate(prevProps, prevState){
-        console.log('=> componentDidUpdate');
+        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
     }
     componentWillUnmount(){
-        console.log('=> componentWillUnmount');        
+        console.log('=> componentWillUnmount(): cleanup before DOM related to component will be removed');        
     }
     render(){
-        console.log('=> render');
+        console.log('return React element to build DOM');
 
         return <div className="number">{this.props.number}</div>;
     }
