e50e7a831519e8331088124d0bbf440092b1b420
mariia <rmashka2001@gmail.com>
2021-10-15 18:19:35 +0300

    8.3

--- lesson8/task3/src/index.jsx
+++ lesson8/task3/src/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import ConnectionStatus from './ConnectionStatus.jsx';
+import Dimensions from './Dimensions.jsx';
 
 const rootElement = document.querySelector('#root');
 
-ReactDOM.render(<ConnectionStatus />, rootElement);
+ReactDOM.render(<Dimensions />, rootElement);
