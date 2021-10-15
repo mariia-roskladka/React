77c2a8e94f273cd8de29d69ade35d7ebfa68b8c7
mariia <rmashka2001@gmail.com>
2021-10-15 11:34:16 +0300

    8.1

--- lesson8/task1/src/Life.jsx
+++ lesson8/task1/src/Life.jsx
@@ -1,28 +1,32 @@
-import React, {Component} from "react";
+import React, { Component } from 'react';
 
-class Life extends Component{
-    constructor(props){
-        super(props);
-        console.log('constructor: good place to create state');
-    }
+class Life extends Component {
+  constructor(props) {
+    super(props);
 
-    componentDidMount(){
-        console.log('componentDidMount: API calls, subscriptions');
-    }
-    shouldComponentUpdate(nextProps, nextState){
-        console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
-    }
-    componentDidUpdate(prevProps, prevState) {
-        // console.log(prevProps, prevState);
-        console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
-      }
-    componentWillUnmount(){
-        console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');        
-    }
-    render(){
-        console.log('return React element to build DOM');
+    console.log('constructor: good place to create state');
+  }
 
-        return <div className="number">{this.props.number}</div>;
-    }
+  componentDidMount() {
+    console.log('componentDidMount: API calls, subscriptions');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    // console.log(nextProps, nextState);
+    console.log('shouldComponentUpdate(nextProps, nextState): decide to render or not to render');
+    return true;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevProps, prevState);
+    console.log('componentDidUpdate(prevProps, prevState): some updates based on new props');
+  }
+  componentWillUnmount() {
+    console.log('componentWillUnmount(): cleanup before DOM related to component will be removed');
+  }
+  render() {
+    console.log('return React element to build DOM');
+
+    return <div className="number">{this.props.number}</div>;
+  }
 }
 export default Life;
