6c3c6e1cc34c45cdf3f047a390a69fefe7caabf2
mariia <rmashka2001@gmail.com>
2021-10-16 10:58:17 +0300

    9.6

---
+++ lesson9/task6/src/index.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable max-classes-per-file */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Game from './Game.jsx';
+
+// ========================================
+
+ReactDOM.render(<Game />, document.getElementById('root'));
