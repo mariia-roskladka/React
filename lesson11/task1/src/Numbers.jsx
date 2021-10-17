b9629a03ca47a8251a5ec575f3f4429c7f1eda8e
mariia <rmashka2001@gmail.com>
2021-10-17 15:04:39 +0300

    11.1

--- lesson11/task1/src/Numbers.jsx
+++ lesson11/task1/src/Numbers.jsx
@@ -1,1 +1,17 @@
-import React from 'react';
+import React, { Component } from "react";
+
+class Numbers extends Component{
+    render(){
+        return(
+            <div className="number">
+                <span className="number__title">
+                    {this.props.title}
+                </span>
+                <span className="number__value">
+                    {this.props.number}
+                </span>
+            </div>
+        );
+    }
+}
+export default Numbers;
